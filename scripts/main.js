781947f2bf48eeed73841a604806a0f484d5b086
Grant Paschal <gpass94@gmail.com>
2017-07-17 16:45:12 -0400

    Modified Commit

--- scripts/main.js
+++ scripts/main.js
@@ -8,26 +8,35 @@
    you'll need to parse through that first before you can start to
    write your logic.
 */
-var cards = []
+var cards = [];
 
-function handValue (hand) {
+function handValue (handArray) {
+let totalofAllCards = 0;
+
+for (let i = 0; i < handArray.length; i++) {
+  if (handArray[i] === "J"|| handArray[i] === "Q"|| handArray[i] === "K" ){
+    //given that comparison is true, then run this process
+    handArray[i]= 10;
+    totalofAllCards += 10;
+  }else if(handArray[i] == "A" && totalofAllCards < 10){
+           handArray[i] = 11;
+           totalofAllCards += 11;
+         }else if(handArray[i] == "A" && totalofAllCards > 10){
+           handArray[i] = 1;
+           totalofAllCards += 1;
+         }else{
+           handArray[i] = parseInt(handArray[i])
+           totalofAllCards += handArray[i];
+         }
+       }
+       if(totalofAllCards > 21){
+         totalofAllCards -= 10;
+       }
+       return totalofAllCards;
+    }
 
 
-for (let i = 0; i < hand.length; i++) {
-  hand[i] ===
-}
-
-  return;
-}
 
 
-/* -----  Hints ------
-
-1..10   ==> Worth face value (1 = 1, 4 = 4, etc)
-K, Q, J ==> Worth 10
-A       ==> Worth 1 or 11
-
-*/
-console.log
-console.assert(handValue(["2", "2", "8"] === 12);
-console.assert(handValue(["2", "2", "K"] === 14);
+console.assert(handValue(["2", "2", "8"] === 12));
+console.assert(handValue(["2", "2", "K"] === 14));
